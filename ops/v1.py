5372b589cb3321b553984305085a06f7c8e436ad
Dominic Jack <thedomjack@gmail.com>
2022-12-01 15:04:05 +1100

    removed header, added types

--- ops/v1.py
+++ ops/v1.py
@@ -1,27 +1,7 @@
-# coding=utf-8
-# Copyright 2022 The Google Research Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Implementation of multiheaded FAVOR-attention & FAVOR-self-attention layers.
-
-Prefix Sum Tensorflow implementation by Valerii Likhosherstov.
-"""
-import typing as tp
 import tensorflow as tf
 
 
-def causal_numerator(qs: tf.Tensor, ks: tf.Tensor, vs: tf.Tensor):
+def causal_numerator(qs: tf.Tensor, ks: tf.Tensor, vs: tf.Tensor) -> tf.Tensor:
     """Computes not-normalized FAVOR causal attention A_{masked}V.
 
     Args:
@@ -39,7 +19,7 @@ def causal_numerator(qs: tf.Tensor, ks: tf.Tensor, vs: tf.Tensor):
     return tf.linalg.matvec(rhs, qs)
 
 
-def causal_denominator(qs, ks):
+def causal_denominator(qs: tf.Tensor, ks: tf.Tensor) -> tf.Tensor:
     """Computes FAVOR normalizer in causal attention.
 
     Args:
